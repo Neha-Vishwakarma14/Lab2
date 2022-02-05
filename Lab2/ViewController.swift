9bd9e832b34ff37c7e8aa1c1fac5bd9a65e0ded6
Neha <nvishwakarma2541@conestogac.on.ca>
2022-02-05 12:42:17 -0500

    Initial Commit

---
+++ Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by user203147 on 2/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
